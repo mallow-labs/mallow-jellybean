b7234cfeb0a1fe199b1a977aebb8eb6092b0d8d3
MeditatingSloth <austin.borden@gmail.com>
2025-07-07 12:45:43 +0700

    more tests

---
+++ clients/umi/test/guards/endDate.test.ts
@@ -0,0 +1,92 @@
+import { drawJellybean } from '@mallow-labs/mallow-gumball';
+import { sol, some } from '@metaplex-foundation/umi';
+import { generateSignerWithSol } from '@metaplex-foundation/umi-bundle-tests';
+import test from 'ava';
+import { fetchJellybeanMachine } from '../../src';
+import {
+  create,
+  createCoreAsset,
+  createUmi,
+  tomorrow,
+  yesterday,
+} from '../_setup';
+
+test('it allows minting before the end date', async (t) => {
+  // Given a gumball machine with a start date in the past.
+  const umi = await createUmi();
+  const assetSigner = await createCoreAsset(umi);
+
+  const jellybeanMachine = await create(umi, {
+    items: [
+      {
+        asset: assetSigner.publicKey,
+      },
+    ],
+    startSale: true,
+    guards: {
+      solPayment: some({ lamports: sol(0.1) }),
+      endDate: some({ date: tomorrow() }),
+    },
+  });
+
+  // When we mint from it.
+
+  // When we mint for another owner using an explicit payer.
+  const buyer = await generateSignerWithSol(umi);
+  const buyerUmi = await createUmi(buyer);
+
+  await drawJellybean(buyerUmi, {
+    jellybeanMachine,
+    mintArgs: {
+      solPayment: some({
+        feeAccounts: [umi.identity.publicKey],
+      }),
+    },
+  }).sendAndConfirm(buyerUmi);
+
+  // Then the mint was successful.
+  const jellybeanMachineAccount = await fetchJellybeanMachine(
+    umi,
+    jellybeanMachine
+  );
+  t.is(jellybeanMachineAccount.supplyRedeemed, 1n);
+});
+
+test('it forbids minting after the end date', async (t) => {
+  // Given a gumball machine with a start date in the past.
+  const umi = await createUmi();
+  const assetSigner = await createCoreAsset(umi);
+
+  const jellybeanMachine = await create(umi, {
+    items: [
+      {
+        asset: assetSigner.publicKey,
+      },
+    ],
+    startSale: true,
+    guards: {
+      solPayment: some({ lamports: sol(0.1) }),
+      endDate: some({ date: yesterday() }),
+    },
+  });
+
+  // When we mint from it.
+
+  // When we mint for another owner using an explicit payer.
+  const buyer = await generateSignerWithSol(umi);
+  const buyerUmi = await createUmi(buyer);
+
+  await t.throwsAsync(
+    drawJellybean(buyerUmi, {
+      jellybeanMachine,
+      mintArgs: {
+        solPayment: some({
+          feeAccounts: [umi.identity.publicKey],
+        }),
+      },
+    }).sendAndConfirm(buyerUmi),
+    {
+      message: /AfterEndDate/,
+    }
+  );
+});
